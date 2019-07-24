6d0161cae2a46ead6b94e4349a79b1eb1d3fafe8
AngelicaSantos <angelica.santos2113@gmail.com>
2019-07-23 20:29:28 -0400

    yikes

--- src/app/model/comment.spec.ts
+++ src/app/model/comment.spec.ts
@@ -1,5 +1,4 @@
-import { Comment } from '../../../../../../angularclient/src/app/model/comment';
-
+// import {Comment} from 
 describe('Comment', () => {
   it('should create an instance', () => {
     expect(new Comment()).toBeTruthy();
